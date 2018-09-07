80f2081ea2071c07f2c74386a0f828ea91f5cd57
Luiz Henrique <luiz.hse@hotmail.com>
2018-09-07 17:25:36 -0300

    Adiciona Estrutura Inicial do Projeto

---
+++ app/src/main/java/com/devlhse/conversormoedas/MainActivity.java
@@ -0,0 +1,31 @@
+package com.devlhse.conversormoedas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+    }
+
+    private static class ViewHolder{
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
+    }
+}
