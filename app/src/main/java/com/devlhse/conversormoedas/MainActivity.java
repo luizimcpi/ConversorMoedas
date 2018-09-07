ca49ad43e12bb39273705180a863e9d8186a8061
Luiz Henrique <luiz.hse@hotmail.com>
2018-09-07 17:34:46 -0300

    Adiciona listener ao botão

--- app/src/main/java/com/devlhse/conversormoedas/MainActivity.java
+++ app/src/main/java/com/devlhse/conversormoedas/MainActivity.java
@@ -2,11 +2,12 @@ package com.devlhse.conversormoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder= new ViewHolder();
 
@@ -20,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textEuro = findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
 
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if(id == R.id.button_calculate){
+            this.mViewHolder.buttonCalculate.setText("Teste");
+        }
     }
 
     private static class ViewHolder{
